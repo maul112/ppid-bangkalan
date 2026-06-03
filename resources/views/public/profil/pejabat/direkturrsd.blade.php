9883f99e702b4fa57789e70457dc53e8e3885379
maul112 <tsukiaka313@gmail.com>
2026-06-04 01:53:55 +0700

    g

--- resources/views/public/profil/pejabat/direkturrsd.blade.php
+++ resources/views/public/profil/pejabat/direkturrsd.blade.php
@@ -1,6 +1,8 @@
-<x-public-layout>
+@extends('layouts.publik')
+
+@section('content')
     <div class="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
         <h1 class="text-3xl font-bold text-gray-800">Profil > Pejabat > Direkturrsd</h1>
         <p class="mt-4 text-gray-600">Halaman ini masih kosong dan sedang dalam tahap pengembangan.</p>
     </div>
-</x-public-layout>
+@endsection
